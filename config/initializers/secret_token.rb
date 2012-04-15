e2a6d5d438e3db28aa157fb9844ce0bca454a177
Pete Williams <petewms1@gmail.com>
2012-04-14 19:57:57 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Steerpike::Application.config.secret_token = '42fe49988f388f04a2c262142b1adf674731c5cbc72c72b44e63956d5bf28206c02a694e4eed134ebb887e4a152a27b957a3b47051423c3b651ec7cffe7fc811'
